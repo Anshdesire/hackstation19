2f8f9e761791ac2c89ffd9f7ac0ff674ecab542c
swapnil <tripatsw@theaa.ie>
2019-05-10 21:51:14 +0530

    updating

---
+++ src/app/graph/graph.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: 'graph.page.html',
+  styleUrls: ['graph.page.scss'],
+})
+export class GraphPage {
+
+}
